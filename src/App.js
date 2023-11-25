5382968374891cc1d253c1cbd91820396438ee9f
Duke-Skygawker <bart3kk990@gmail.com>
2023-11-25 19:11:49 +0100

    navigation restructuring

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import Navbar from "./components/Navbar";
+import Navbar from "./components/page_navigation/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Services from "./components/Services";
 import Tours from "./components/tours/Tours";
-import Footer from "./components/Footer";
+import Footer from "./components/page_navigation/Footer";
 
 function App() {
   return (
---
+++ src/components/page_navigation/pageLinksObj.js
@@ -0,0 +1,3 @@
+const pagelinks = [];
+
+export default links;
