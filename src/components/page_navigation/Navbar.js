83e0e7a49e65135fe038554707e31a8b59aa2844
Duke-Skygawker <bart3kk990@gmail.com>
2023-12-17 12:18:20 +0100

    fixed navbar transition bug

--- src/components/page_navigation/Navbar.js
+++ src/components/page_navigation/Navbar.js
@@ -3,13 +3,40 @@ import pageLinks from "./pageLinksObj";
 import socialLinks from "./socialLinksObj";
 import Navli from "./Navli";
 import SocialLi from "./SocialLi";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 
 const Navbar = () => {
   const [showNavbar, setShowNavbar] = useState(false);
+  // const [linksHeight, setLinksHeight] = useState(0);
+  // const [linksMargin, setLinksMargin] = useState(0);
+  const [navWidth, setNavWidth] = useState(window.innerWidth);
+  const navRef = useRef(null);
+  // const linksRef = useRef(null);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const navWidth = navRef.current.getBoundingClientRect().width;
+      if (navWidth >= 992) {
+        setShowNavbar(false);
+      }
+      setNavWidth(navWidth);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const expandLinks = () => {
+    // const linksCont = linksRef.current.getBoundingClientRect();
+    // setLinksHeight(linksCont.x);
+    // setLinksMargin(linksCont.top * 2);
+    setShowNavbar(!showNavbar);
+  };
+
   return (
     <>
-      <nav className="navbar">
+      <nav className="navbar" ref={navRef}>
         <div className="nav-center">
           <div className="nav-header">
             <img src={logo} className="nav-logo" alt="backroads" />
@@ -17,13 +44,19 @@ const Navbar = () => {
               type="button"
               className="nav-toggle"
               id="nav-toggle"
-              onClick={() => setShowNavbar(!showNavbar)}
+              onClick={expandLinks}
             >
               <i className="fas fa-bars"></i>
             </button>
           </div>
           <ul
-            className={showNavbar ? "nav-links show-links" : "nav-links"}
+            className={
+              showNavbar
+                ? navWidth >= 992
+                  ? "nav-links"
+                  : "nav-links show-links"
+                : "nav-links"
+            }
             id="nav-links"
           >
             {pageLinks.map((link) => (
