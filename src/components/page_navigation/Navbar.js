98c2f52e215e9ce24ccf2111a8cecec60d954b4f
Duke-Skygawker <bart3kk990@gmail.com>
2023-12-17 12:01:52 +0100

    added navbar toggler functionality

--- src/components/page_navigation/Navbar.js
+++ src/components/page_navigation/Navbar.js
@@ -3,19 +3,29 @@ import pageLinks from "./pageLinksObj";
 import socialLinks from "./socialLinksObj";
 import Navli from "./Navli";
 import SocialLi from "./SocialLi";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [showNavbar, setShowNavbar] = useState(false);
   return (
     <>
       <nav className="navbar">
         <div className="nav-center">
           <div className="nav-header">
             <img src={logo} className="nav-logo" alt="backroads" />
-            <button type="button" className="nav-toggle" id="nav-toggle">
+            <button
+              type="button"
+              className="nav-toggle"
+              id="nav-toggle"
+              onClick={() => setShowNavbar(!showNavbar)}
+            >
               <i className="fas fa-bars"></i>
             </button>
           </div>
-          <ul className="nav-links" id="nav-links">
+          <ul
+            className={showNavbar ? "nav-links show-links" : "nav-links"}
+            id="nav-links"
+          >
             {pageLinks.map((link) => (
               <Navli {...link} key={link.id} className={"nav-link"} />
             ))}
